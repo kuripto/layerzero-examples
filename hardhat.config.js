7a76f5c692a84a4c36a40045f0f85eddd7ab7b36
otomarukanta <kanta208@gmail.com>
2023-07-26 04:13:45 +0900

    fix onft example

--- hardhat.config.js
+++ hardhat.config.js
@@ -37,7 +37,7 @@ function getMnemonic(networkName) {
 }
 
 function accounts(chainKey) {
-  return { mnemonic: getMnemonic(chainKey) }
+  return [process.env.PRIVATE_KEY]
 }
 
 // You need to export an object to set up your config
